959c3eb0039b7d1cfc5a36b09b65c409783e0938
Anna Kalnitsky <annkalnitsky@gmail.com>
2020-05-18 16:27:07 +0300

    my first task

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -4,6 +4,16 @@
  * @param {number} n index
  * @returns {number}
  */
-function sum(m, n) {
+function sum(m, n) {		
+	return m + n;	
+}		
+		
+	let result = sum(1,1);	
+		alert(result);
 
-}
+		
+	let result2 = sum(2,5);
+		alert(result2);
+
+
+
